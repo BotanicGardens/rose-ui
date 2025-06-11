e3ca486fdd3d8eb008b5a4076be578362b159929
Alicehhhmm <a007aplum@gmail.com>
2025-06-11 23:32:10 +0800

    chore(config): update tsconfig

---
+++ packages/rose-tsconfig/README.md
@@ -0,0 +1,3 @@
+# Rose Tsconfig
+
+An internal library providing tsconfig for Rose UI
---
+++ packages/rose-tsconfig/index.js
@@ -0,0 +1,9 @@
+import _base from './base.json' with { type: 'json' }
+import _next from './next.json' with { type: 'json' }
+import _react from './react.json' with { type: 'json' }
+import _vite_react from './vite-react.json' with { type: 'json' }
+
+export const tsconfigBase = _base
+export const tsconfigReact = _react
+export const tsconfigViteReat = _vite_react
+export const tsconfigNext = _next
