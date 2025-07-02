439b3281fb896deb6438ea93a85428f4646008c4
Alicehhhmm <a007aplum@gmail.com>
2025-07-02 21:22:08 +0800

    chore(storybook): init storybook

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from 'eslint-plugin-storybook'
+
 // @ts-check
 import eslint from '@eslint/js'
 import tseslint from 'typescript-eslint'
@@ -13,5 +16,6 @@ export default tseslint.config(
             '**/playground-temp/**',
             '**/temp/**',
         ],
-    }
+    },
+    storybook.configs['flat/recommended']
 )
